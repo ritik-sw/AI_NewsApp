b721b31a33116e72df6a7e8dd25b3eb54cecd1e5
Ritik5Prasad <rv724405@gmail.com>
2022-09-13 16:27:03 +0530

    Updates

---
+++ precache-manifest.ed5e3135be90dff786a9900ff7d8a3c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11eda5fa2c925f85cf6f53e82bb051ad",
+    "url": "/AI_NewsApp/index.html"
+  },
+  {
+    "revision": "769f3af2d2668cee9936",
+    "url": "/AI_NewsApp/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "9941b6472f5ac7bf1176",
+    "url": "/AI_NewsApp/static/js/2.e3c75ac9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AI_NewsApp/static/js/2.e3c75ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769f3af2d2668cee9936",
+    "url": "/AI_NewsApp/static/js/main.4c505878.chunk.js"
+  },
+  {
+    "revision": "2086b9fd0c3ddc575a60",
+    "url": "/AI_NewsApp/static/js/runtime-main.dff38ff2.js"
+  },
+  {
+    "revision": "720a7192bae880edc8df66bd48324c1b",
+    "url": "/AI_NewsApp/static/media/logo.720a7192.png"
+  }
+]);
